703048f392f8372e5739bdcf01f39eb5e74ed9c6
Eliton-jpg <elitonlima.ti@gmail.com>
2023-10-26 14:56:41 -0300

    changes

--- user_data.sh
+++ user_data.sh
@@ -9,8 +9,8 @@ curl -sL "https://raw.githubusercontent.com/Eliton-jpg/Atividade-Docker-Compasso
 yum install nfs-utils -y
 mkdir /mnt/efs/
 chmod +rwx /mnt/efs/
-mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-07c68e847f4ea9744.efs.us-east-1.amazonaws.com:/ /mnt/efs/
-echo "fs-07c68e847f4ea9744.efs.us-east-1.amazonaws.com:/ /mnt/efs nfs defaults 0 0" >> /etc/fstab
+mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-0104dbb46333af958.efs.us-east-1.amazonaws.com:/ /mnt/efs/
+echo "fs-0104dbb46333af958.efs.us-east-1.amazonaws.com:/ /mnt/efs nfs defaults 0 0" >> /etc/fstab
 usermod -aG docker ${USER}
 chmod 666 /var/run/docker.sock
 docker-compose -f /home/ec2-user/docker-compose.yaml up -d
